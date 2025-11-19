fa3159806e652b60163a1cda884ed277f7ac2b77
Sarah Schwarz <sarahschwarza@gmail.com>
2025-11-19 15:34:35 -0800

    reformat

--- tester.py
+++ tester.py
@@ -7,28 +7,28 @@ import numpy as np
 import argparse
 import pathlib
 
-parser = argparse.ArgumentParser(description = "arguement parsing:", prog='PROG', usage='%(prog)s [options]')
-
-parser.add_argument("TRANSITION_TEMP", help ="Transition temperature", type = float)          # Warm to this with zero field, zero voltage
-parser.add_argument("TEMPS", help ="Range of temperatures in Kelvin: ex. [50, 10, 2]", type = float, nargs='*')
-parser.add_argument("VAS", help ="Voltages on CH1 Tension (see temperature dependant limitations in Razorbill User Guide): ex. [0, 5, 20, 50]", type = float, nargs='*')
-parser.add_argument("VBS", help ="Voltages on CH2 Compression(see temperature dependant limitations in Razorbill User Guide): ex. [10, 10, 10, 10]", type = float, nargs='*')
-parser.add_argument("FIELD", help ="max field (Oe), min field (Oe), ramping rate (Oe/sec): ex. 90000, -90000, 50", type = float, nargs=3)
-parser.add_argument("QD_FILES", help ="Folder Path:  ", type=pathlib.Path)
+parser = argparse.ArgumentParser(description = "arguement parsing:"')
 args = parser.parse_args()
-
+parser.add_argument("TRANSITION_TEMP", help ="Transition temperature", type = float)          # Warm to this with zero field, zero voltage
 if args.TRANSITION_TEMP is None:
-    args.TRANSITION_TEMP = input(args.TRANSITION_TEMP.help())
+    args.TRANSITION_TEMP = input(args.TRANSITION_TEMP.help())                        
+parser.add_argument("TEMPS", help ="Range of temperatures in Kelvin: ex. [50, 10, 2]", type = float, nargs='*')
 if args.TEMPS is None:
     args.TEMPS = input(args.TEMPS.help())
+parser.add_argument("VAS", help ="Voltages on CH1 Tension (see temperature dependant limitations in Razorbill User Guide): ex. [0, 5, 20, 50]", type = float, nargs='*')
 if args.VAS is None:
     args.VAS = input(args.VAS.help())
+parser.add_argument("VBS", help ="Voltages on CH2 Compression(see temperature dependant limitations in Razorbill User Guide): ex. [10, 10, 10, 10]", type = float, nargs='*')
 if args.VBS is None:
     args.VBS = input(args.VBS.help())
+parser.add_argument("FIELD", help ="max field (Oe), min field (Oe), ramping rate (Oe/sec): ex. 90000, -90000, 50", type = float, nargs=3)
 if args.FIELD is None:
     args.FIELD = input(args.FIELD.help())
+parser.add_argument("QD_FILES", help ="Folder Path:  ", type=pathlib.Path)
 if args.QD_FILES is None:
     args.QD_FILES  = input(args.QD_FILES.help())
+args = parser.parse_args()
+
 
 
 print('list inputs:')
@@ -36,6 +36,7 @@ print("args.TRANSITION_TEMP:")
 print(args.TRANSITION_TEMP)
 print("args.TEMPS:")
 print(args.TEMPS)
+
 
 
 
