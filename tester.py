4c3892ff8caaaf8b870f7faada3244882df961cd
Sarah Schwarz <sarahschwarza@gmail.com>
2025-11-19 15:32:01 -0800

    Refactor argument parser with usage and program name

--- tester.py
+++ tester.py
@@ -7,7 +7,7 @@ import numpy as np
 import argparse
 import pathlib
 
-parser = argparse.ArgumentParser(description = "arguement parsing:")
+parser = argparse.ArgumentParser(description = "arguement parsing:", prog='PROG', usage='%(prog)s [options]')
 
 parser.add_argument("TRANSITION_TEMP", help ="Transition temperature", type = float)          # Warm to this with zero field, zero voltage
 parser.add_argument("TEMPS", help ="Range of temperatures in Kelvin: ex. [50, 10, 2]", type = float, nargs='*')
@@ -36,6 +36,7 @@ print("args.TRANSITION_TEMP:")
 print(args.TRANSITION_TEMP)
 print("args.TEMPS:")
 print(args.TEMPS)
+
 
 
 
