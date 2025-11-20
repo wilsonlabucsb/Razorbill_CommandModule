3bee738f77c4075081c8aed99912ed2d12e2be0c
Sarah Schwarz <sarahschwarza@gmail.com>
2025-11-19 18:00:29 -0800

    fixes command line prompting

--- field_main.py
+++ field_main.py
@@ -15,7 +15,7 @@ parser = argparse.ArgumentParser(description = "arguement parsing:")
 parser.add_argument('--nul', type = float, default = 0)
 
 parser.add_argument('--TEMPS', type = float, nargs='*', required = False)
-parser.add_argument('--VAS', help ="Voltages on CH1 Tension (see temperature dependant limitations in Razorbill User Guide): ex. [0, 5, 20, 50]", type = float, nargs='*', required = False)
+parser.add_argument('--VAS', type = float, nargs='*', required = False)
 parser.add_argument('--VBS', type = float, nargs='*', required = False)
 parser.add_argument('--FIELD', type = float, nargs=3, required = False)
 parser.add_argument('--QD_FILES', type=pathlib.Path, required = False)
@@ -25,15 +25,22 @@ args = parser.parse_args()
 print("This will run Field Sweeps at a list of given Temperatures for each Razorbill Power Setting VAS,VBS supplied:")
                
 if args.TEMPS is None:
-    args.TEMPS = input("Range of temperatures in Kelvin: ex. [50, 10, 2]:  ")
+    args.TEMPS = input("Range of temperatures in Kelvin: ex. 50 10 2 :  ")
+    args.TEMPS = np.array(args.TEMPS)
 if args.VAS is None:
-    args.VAS = input("Voltages on CH1 Tension (see temperature dependant limitations in Razorbill User Guide): ex. [0, 5, 20, 50]:  ")
+    args.VAS = input("Voltages on CH1 Tension (see temperature dependant limitations in Razorbill User Guide): ex. 0 5 20 50 :  ")
+    args.VAS = np.array(args.VAS)
 if args.VBS is None:
-    args.VBS = input("Voltages on CH2 Compression(see temperature dependant limitations in Razorbill User Guide): ex. [10, 10, 10, 10]:  ")
+    args.VBS = input("Voltages on CH2 Compression(see temperature dependant limitations in Razorbill User Guide): ex. 10 10 10 10 :  ")
+    args.VBS = np.array(args.VBS)
 if args.FIELD is None:
-    args.FIELD = input("max field (Oe), min field (Oe), ramping rate (Oe/sec): ex. 90000, -90000, 50:  ")
+    args.FIELD = input("max field (T), min field (T), ramping rate (Oe/sec): ex. 9 -9 50 :  ")
+    args.FIELD = np.array(args.FIELD)
+    args.FIELD[0] *= 10000 #unit conversion
+    args.FIELD[1] *= 10000 #unit conversion
 if args.QD_FILES is None:
     args.QD_FILES  = input("Folder Path:  ")
+    args.QD_FILES = str(args.QD_FILES)
 
 
 QD_FILE = max(glob.glob(args.QD_FILES+"*.dat"), key=os.path.getctime)
