2d14adbc2ddea4d5966cd3f042fbdabcc46fd0e9
Sarah Schwarz <sarahschwarza@gmail.com>
2025-11-19 16:07:35 -0800

    Refactor argument parsing in tester.py

--- tester.py
+++ tester.py
@@ -8,7 +8,7 @@ import argparse
 import pathlib
 
 parser = argparse.ArgumentParser(description = "arguement parsing:")
-parser.add_argument('nul', type = float, default = 0)
+parser.add_argument('--nul', type = float, default = 0)
 
 args = parser.parse_args()
 parser.add_argument('TRANSITION_TEMP', type = float, required = False)          # Warm to this with zero field, zero voltage
@@ -39,6 +39,7 @@ print("args.TRANSITION_TEMP:")
 print(args.TRANSITION_TEMP)
 print("args.TEMPS:")
 print(args.TEMPS)
+
 
 
 
